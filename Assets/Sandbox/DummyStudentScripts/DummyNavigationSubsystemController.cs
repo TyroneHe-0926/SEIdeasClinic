af23f3e9287bc92f5810f3ccb72847ccb221fdfe
hahafhaha <59632128+hahafhaha@users.noreply.github.com>
2020-01-07 18:56:48 -0800

    Add files via upload

---
+++ Assets/Sandbox/DummyStudentScripts/DummyNavigationSubsystemController.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// This is stub code to be copied into the students' "blank" project. 
+/// This is their starting point for the activity.
+/// It should overwrite the contents of "NavigationSubsystemController.cs"
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+using Sandbox;
+
+public class DummyNavigationSubsystemController
+{
+    public void NavigationUpdate(SubsystemReferences SystemReferences, GalaxyMapData galaxyMapData)
+    {
+        
+
+    }   
+}
---
+++ Assets/Sandbox/DummyStudentScripts/DummySensorsSubsystemController.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// This is stub code to be copied into the students' "blank" project. 
+/// This is their starting point for the activity.
+/// It should overwrite the contents of of the appropriate non-dummy subsystemcontroller script.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+using Sandbox;
+
+public class DummySensorSubsystemController
+{    
+    public void SensorsUpdate(SubsystemReferences subsysRef, ShipSensors Data)
+    {
+        
+    }
+}
