09e22bb6d96171347f32f0ba61e2c3ae73b5471d
hahafhaha <59632128+hahafhaha@users.noreply.github.com>
2020-01-07 18:41:08 -0800

    Add files via upload

---
+++ Assets/Sandbox/Scripts/UI/MissionAccomplishedPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionAccomplishedPanel : MonoBehaviour {
+
+    [Header("References")]
+    [SerializeField]
+    Text titleText;
+    [SerializeField]
+    Text bodyText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetTitleText(string msg)
+    {
+        titleText.text = msg;
+    }
+
+    public void SetBodyText(string msg)
+    {
+        bodyText.text = msg;
+    }
+}
